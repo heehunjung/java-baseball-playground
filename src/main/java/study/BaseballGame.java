50aeed1ae2430e13654af19378f77865c1b446f5
heehun <slrkqk2@naver.com>
2024-09-03 13:53:33 +0900

    feat: input view, compare 로직

--- src/main/java/study/BaseballGame.java
+++ src/main/java/study/BaseballGame.java
@@ -1,21 +1,76 @@
 package study;
 
+import java.util.Objects;
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseballGame {
     public static void InputView() {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
-    public static void ResultView() {
-
+    public static void ResultView(int[] result) {
+        if (result[0]==3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return;
+        }
+        if (result[1]!=0) {
+            System.out.print(result[1]+"볼");
+        }
+        if (result[0]!=0) {
+            System.out.print(result[0]+"스트라이크");
+        }
     }
 
-    public static int RandomNumber() {
-        Random random = new Random();
-        //이 프로그램에서 랜덤 객체 주입하는 부분을 다른 메소드로 분리하는게 나을라나?
+    public static int[] CompareNumber(String random, String userNumber) {
+        String[] randoms = random.split("");
+        String[] userNumbers = userNumber.split("");
 
+        int sameNumber = 0;
+        int exactlySameNumber = 0;
+        for (int i = 0; i < randoms.length; i++) {
+            exactlySameNumber += CheckExactly(randoms[i], userNumbers[i]);
+            sameNumber += CheckSame(randoms[i],userNumber);
+        }
+        sameNumber -= exactlySameNumber;
+
+        int[] result = new int[2];
+        result[0] = exactlySameNumber;
+        result[1] = sameNumber;
+
+        return result;
+    }
+
+    public static int CheckExactly(String random, String userNumber) {
+        if (Objects.equals(random, userNumber)){
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int CheckSame(String random, String userNumber) {
+        String[] userNumbers = userNumber.split("");
+        int sameNumber = 0;
+        for (String number : userNumbers) {
+            sameNumber += CheckExactly(random, number);
+        }
+        return sameNumber;
+    }
+
+
+    public static String RandomNumber(Random random) {
         // 100부터 999 사이의 랜덤한 정수를 반환 (3자리 숫자)
-        return random.nextInt(900) + 100;
+        return String.valueOf(random.nextInt(900) + 100);
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        String randomNumber = RandomNumber(random);
+        while(true) {
+            InputView();
+            String userChoice = sc.nextLine();
+
+        }
     }
 }
--- src/test/java/study/BaseballGameTest.java
+++ src/test/java/study/BaseballGameTest.java
@@ -3,26 +3,42 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static study.BaseballGame.RandomNumber;
+import static study.BaseballGame.*;
 
 public class BaseballGameTest {
-
-    @DisplayName("InputView 테스트")
+    @DisplayName("CheckExactly 테스트")
     @Test
-    void inputViewTest() {
+    void CheckExactlyTest() {
+        assertThat(CheckExactly("1","1")).isEqualTo(1);
+        assertThat(CheckExactly("1","2")).isEqualTo(0);
+    }
 
+    @DisplayName("CheckSame 테스트")
+    @Test
+    void CheckSameTest() {
+        assertThat(CheckSame("1","123")).isEqualTo(1);
+        assertThat(CheckSame("5","155")).isEqualTo(2);
+    }
+    @DisplayName("CompareNumber 테스트")
+    @Test
+    void CompareNumberTest() {
+        int[] result = CompareNumber("123", "321");
+        assertThat(result).containsExactly(1, 2); // 배열의 내용을 직접 비교
     }
     @DisplayName("ResultView 테스트")
     @Test
-    void resultViewTest() {
+    void ResultViewTest() {
 
     }
     @DisplayName("RandomNumber 테스트")
     @Test
-    void randomNumberTest() {
-        int first = RandomNumber();
-        int second = RandomNumber();
+    void RandomNumberTest() {
+        Random random = new Random();
+        String first = RandomNumber(random);
+        String second = RandomNumber(random);
         assertThat(first).isNotEqualTo(second);
     }
 }
